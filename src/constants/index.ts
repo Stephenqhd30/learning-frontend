d056054196576c67084bf023fb8a778dce3a16a0
Stephenqhd30 <Popcornqhd@gmail.com>
2024-10-17 21:24:54 +0800

    feat(config):添加多环境配置

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const LEARNING_TITLE = '证书查询系统';
 export const LEARNING_SUBTITLE = '河南开封科技传媒学院继续教育学院';
 export const LEARNING_DEFAULT_MESSAGE = '继续教育学院';
 export const BASE_URL = 'http://localhost:8080';
+export const PRODUCTION_URL = 'http://1.13.163.104:8080';
 export const ACCOUNT_TITLE = "我的城堡说欢迎光临";
 export const INDEX_PAGE_TITLE = "河南开封科技传媒学院继续教育学院证书查询";
 export const MY_CERTIFICATE_TITLE = "河南开封科技传媒学院继续教育学院我的证书";
