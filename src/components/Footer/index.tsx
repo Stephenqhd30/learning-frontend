3e387decfa1267dd07a82cf6791733d54ba67539
stephenqiu <Popcornqhd@gmail.com>
2025-06-16 19:07:33 +0800

    feat:添加系统备案号

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,45 @@
-import { DefaultFooter } from '@ant-design/pro-components';
+import {DefaultFooter} from '@ant-design/pro-components';
 import React from 'react';
-import {LEARNING_DEFAULT_MESSAGE, LEARNING_IPC, LEARNING_URL} from '@/constants';
+import {
+  LEARNING_DEFAULT_MESSAGE,
+  LEARNING_IPC,
+  LEARNING_IPC_URL,
+  LEARNING_PUBLIC,
+  LEARNING_PUBLIC_URL
+} from '@/constants';
+import {GlobalOutlined, SafetyCertificateOutlined} from '@ant-design/icons';
 
 const Footer: React.FC = () => {
-  const defaultMessage = LEARNING_DEFAULT_MESSAGE;
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
-      copyright={`${defaultMessage} ${currentYear}`}
+      copyright={`${LEARNING_DEFAULT_MESSAGE} ${currentYear}`}
       style={{
-        background: 'none',
+        background: 'none'
       }}
       links={[
         {
           key: 'LEARNING_IPC',
-          title: LEARNING_IPC,
-          href: LEARNING_URL,
-          blankTarget: true,
+          title: (
+            <>
+              <SafetyCertificateOutlined style={{marginRight: 4}}/>
+              备案号: {LEARNING_IPC}
+            </>
+          ),
+          href: LEARNING_IPC_URL,
+          blankTarget: true
         },
+        {
+          key: 'LEARNING_PUBLIC',
+          title: (
+            <>
+              <GlobalOutlined style={{marginRight: 4}}/>
+              {LEARNING_PUBLIC}
+            </>
+          ),
+          href: LEARNING_PUBLIC_URL,
+          blankTarget: true
+        }
       ]}
     />
   );
